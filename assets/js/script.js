62079648744bf4dea57fba19f81610aa43dfbf72
unknown <blancardt@merrimack.edu>
2023-08-10 20:29:48 -0400

    created an on click event for the search button to display character info based off search bar text.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,53 @@
-//declare the url, public and private key in variables.
-var url = 'https://gateway.marvel.com:443/v1/public/characters?name=';
-var publicKey = "e2e4e185f80f3a71cdcff67fb6afc597";
-var privateKey = "504281b22311ba46a13b1314cb218178efc48b84";
-//get the current unix code for the time stamp.
-var ts = dayjs().unix();
+$(function() {
+  //declare the url, public and private key in variables.
+  var url = 'https://gateway.marvel.com:443/v1/public/characters?name=';
+  var publicKey = "e2e4e185f80f3a71cdcff67fb6afc597";
+  var privateKey = "504281b22311ba46a13b1314cb218178efc48b84";
+  //get the current unix code for the time stamp.
+  var ts = dayjs().unix();
 
-//Went to https://stackoverflow.com/questions/1655769/fastest-md5-implementation-in-javascript to figure out how to
-//use the MD5 function.  Also credited in the readme file.
-var hash = CryptoJS.MD5(ts + privateKey + publicKey).toString();
-//replace Hulk with input value from the search box.  Place this in a function later.
-var requestUrl = url + "Hulk" + "&ts=" + ts + "&apikey=" + publicKey + "&hash=" + hash;
+  //store specific elements from the html to the variables.
+  var searchBar = $("#searchTerm");
+  var searchButtonEl = $("#searchCharacter");
 
-console.log(requestUrl);
+  //Went to https://stackoverflow.com/questions/1655769/fastest-md5-implementation-in-javascript to figure out how to
+  //use the MD5 function.  Also credited in the readme file.
+  var hash = CryptoJS.MD5(ts + privateKey + publicKey).toString();
+  //replace Hulk with input value from the search box.  Place this in a function later.
+  var requestUrl;
 
-function createImage(targetedResult) {
-  //get the image url info from the api.
-  var link = targetedResult.thumbnail.path + "." + targetedResult.thumbnail.extension;
 
-  //remove the http from the begining o fthe string and change it to https for secure connection.
-  //Went to https://bobbyhadz.com/blog/javascript-remove-first-n-characters-from-string to figure out how to remove 
-  //the first 4 characters of a string.  Also credited in the readme file.
-  var imageUrl = "https" + link.slice(4);
 
-  return imageUrl;
-}
+  function createImage(targetedResult) {
+    //get the image url info from the api.
+    var link = targetedResult.thumbnail.path + "." + targetedResult.thumbnail.extension;
 
-fetch(requestUrl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
+    //remove the http from the begining o fthe string and change it to https for secure connection.
+    //Went to https://bobbyhadz.com/blog/javascript-remove-first-n-characters-from-string to figure out how to remove 
+    //the first 4 characters of a string.  Also credited in the readme file.
+    var imageUrl = "https" + link.slice(4);
 
-    var dataResult = data.data.results[0]
+    return imageUrl;
+  }
 
+  function determineIfCharacter(data) {
+    if (data.data.results.length > 0) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  function searchResultMarvelUpdate (characterInfo) {
     //get the image url.
-    var imageUrl = createImage(dataResult);
+    var imageUrl = createImage(characterInfo);
 
     //get character name and description and store them in a variable.
-    var name = dataResult.name;
-    var description = dataResult.description;
+    var name = characterInfo.name;
+    var description = characterInfo.description;
+
+    //create a function to check if this hero has been saved to local storage.
 
     //update the index.html file using jquerry to add the picture, description, and image appropriatly.
     var topImage = $(".card-img-top");
@@ -51,5 +59,43 @@ fetch(requestUrl)
 
     var characterCardText = $(".card-text");
     characterCardText.text(description);
+  }
 
-  });
+  function getSearchText(event) {
+    event.preventDefault();
+    var searchResult = searchBar.val();
+    requestUrl = url + searchResult + "&ts=" + ts + "&apikey=" + publicKey + "&hash=" + hash;
+    console.log(requestUrl);
+    fetch(requestUrl)
+      .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      //check if valid URL.
+      if(data.code != 200) {
+        //create a function that displays a character not found where the character info should be.
+        return;
+      }
+      var characterFound = determineIfCharacter(data);
+      console.log(characterFound);
+      if (characterFound) {
+        //get the first value in the array.
+        var characterResult = data.data.results[0];
+        //call function to deal with the marvel api.
+        searchResultMarvelUpdate(characterResult);
+      }
+      else {
+        //create a function that displays a character not found where the character info should be.
+        return;
+      }
+
+      //create a function that deals with wikipedia api.
+
+    });
+
+  }
+
+  searchButtonEl.on("click", getSearchText);
+  //create an on click event for the favorite button.
+});
